c88c20466a1c5f83159b12ef270ba0a2c409e36f
Takashi Kikuchi <tkskkc@gmail.com>
2025-11-14 00:58:28 +0900

    feat: insert_snipを分離

--- src/snip/main.py
+++ src/snip/main.py
@@ -8,47 +8,17 @@ Usage:
   cat file | snip.py add  # read body from stdin
 """
 import argparse
-import json
 import snip.constants as C
 import sys
 from . import __version__
 from snip.snip_list import fzf_select, rofi_name
+from snip.snip_add  import insert_snip
 from sqlite_utils   import Database
-
-
-def insert_snip(db: Database, args, body: str):
-    trigger = args.trigger or (None if not body else body.splitlines()[0][:40])
-    tags = [t for t in args.tags.split(",") if t]
-
-    with db.conn:
-        if args.id:
-            db[C.TABLE].upsert({
-                "id":      args.id,
-                "trigger": trigger,
-                "body":    body,
-                "memo":    args.memo,
-                "abbr":    args.abbr,
-                "tags":    json.dumps(tags),
-                "mode":    args.mode,
-            }, pk="id")
-        else:
-            db[C.TABLE].insert({
-                "trigger": trigger,
-                "body":    body,
-                "memo":    args.memo,
-                "abbr":    args.abbr,
-                "tags":    json.dumps(tags),
-                "mode":    args.mode,
-            }, pk="id", columns={"abbr": int})
-        ret = db[C.TABLE].get(db.conn.execute("SELECT last_insert_rowid()").fetchone()[0])
-
-    return ret
 
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument("--version", action="version",
-                   version=f"%(prog)s {__version__}",
+    p.add_argument("--version", action="version", version=f"%(prog)s {__version__}",
                    help="バージョン表示")
 
     sub    = p.add_subparsers(dest="cmd")
