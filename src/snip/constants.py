aef9e80288132dc17247f7794729c889f98d523e
Takashi Kikuchi <tkskkc@gmail.com>
2025-11-15 21:33:39 +0900

    chore: mod constants

--- src/snip/constants.py
+++ src/snip/constants.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
-DBNAME    = "snippets.db"
-TABLE     = "snippets"
-TAG_TABLE = "tags"
+DBNAME         = "snippets.db"
+TABLE          = "snippets"
+TAG_TABLE      = "tags"
 
+FISH_ABBR_FILE = "abbr.fish"
+
+# Class : Path
 CNF_PATH  = Path.home() / ".config"
 DB_PATH   = CNF_PATH / "mine"
 DB_FILE   = DB_PATH / DBNAME
 NVIM_SNIP = CNF_PATH / "nvim" / "luasnippets"
-FISH_ABBR = CNF_PATH / "fish" / "abbr.fish"
+FISH_ABBR = CNF_PATH / "fish" / FISH_ABBR_FILE
