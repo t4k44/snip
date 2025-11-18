5a6442b30e88b2b6faedd88fe6e4d655ac0b556e
Takashi Kikuchi <tkskkc@gmail.com>
2025-11-18 17:26:17 +0900

    feat: argsから必要フィールドを抽出しdbを更新するよう変更

--- src/snip/snippets.py
+++ src/snip/snippets.py
@@ -1,6 +1,9 @@
-import json
 import snip.constants as C
 from sqlite_utils import Database
+
+
+TARGET_FIELD = ["trigger", "body", "memo", "abbr", "tags", "mode"]
+
 
 def dict_pickup(target: dict, fields: list):
     """dict targetからfieldsを抽出。値がNoneの場合は除外"""
@@ -12,30 +15,22 @@ def dict_pickup(target: dict, fields: list):
     return ret_dict
 
 
-def insert(db: Database, args, body: str):
-    trigger = args.trigger or (None if not body else body.splitlines()[0][:40])
-    tags    = [t for t in args.tags.split(",") if t]
+def insert(db: Database, args):
+    args.trigger = args.trigger or "memo"
+    args.tags = [t for t in args.tags.split(",") if t]
 
     with db.conn:
-        db[C.TABLE].insert({
-            "trigger": trigger,
-            "body":    body,
-            "memo":    args.memo,
-            "abbr":    args.abbr,
-            "tags":    json.dumps(tags),
-            "mode":    args.mode,
-        }, pk="id", columns={"abbr": int})
+        db[C.TABLE].insert(dict_pickup(vars(args), TARGET_FIELD), pk="id", columns={"abbr": int})
         ret = db[C.TABLE].get(db.conn.execute("SELECT last_insert_rowid()").fetchone()[0])
 
     return ret
 
 
 def update(db: Database, args):
-    target_field = ["trigger", "body", "memo", "abbr", "tags", "mode"]
 
     with db.conn:
         target = db[C.TABLE].get(args.id)
-        target.update(dict_pickup(vars(args), target_field))
+        target.update(dict_pickup(vars(args), TARGET_FIELD))
         target.update({"tags": [t for t in args.tags.split(",") if t]})
 
         db[C.TABLE].upsert(target, pk="id")
